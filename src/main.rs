60d6647281a74299ca9b6cb9bb13b7c0c1a69297
Samuel Ortiz <sameo@linux.intel.com>
2021-02-06 19:27:20 +0100

    do-core1: Add negative unit tests
    Now that we know if disassembling reported an error or not, we can
    implement negative unit testing.
    
    Signed-off-by: Samuel Ortiz <sameo@linux.intel.com>

--- src/main.rs
+++ src/main.rs
@@ -131,6 +131,22 @@ mod tests {
     }
 
     #[test]
+    fn test_instruction_disassemble_add_r9_r1() -> Result<(), Error> {
+        let insn_bytes: u16 = 0x291;
+        assert!(Instruction::disassemble(insn_bytes).is_err());
+
+        Ok(())
+    }
+
+    #[test]
+    fn test_instruction_disassemble_add_r0_r10() -> Result<(), Error> {
+        let insn_bytes: u16 = 0x20a;
+        assert!(Instruction::disassemble(insn_bytes).is_err());
+
+        Ok(())
+    }
+
+    #[test]
     fn test_instruction_disassemble_add_r7_r2() -> Result<(), Error> {
         let insn_bytes: u16 = 0x272;
         let insn = Instruction::disassemble(insn_bytes)?;
