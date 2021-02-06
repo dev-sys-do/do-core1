ae1b7a4ccf75296c632b935fba0ed74797e6b514
Samuel Ortiz <sameo@linux.intel.com>
2021-02-06 17:54:17 +0100

    do-core1: Add more Instruction unit tests
    We add more tests for disassembling all supported instructions.
    
    Signed-off-by: Samuel Ortiz <sameo@linux.intel.com>

--- src/main.rs
+++ src/main.rs
@@ -100,4 +100,44 @@ mod tests {
         assert_eq!(insn.op0, 1);
         assert_eq!(insn.op1, 3);
     }
+
+    #[test]
+    fn test_instruction_disassemble_add_r7_r2() {
+        let insn_bytes: u32 = 0x11c2;
+        let insn = Instruction::disassemble(insn_bytes);
+
+        assert_eq!(insn.opcode, OpCode::ADD);
+        assert_eq!(insn.op0, 7);
+        assert_eq!(insn.op1, 2);
+    }
+
+    #[test]
+    fn test_instruction_disassemble_ldw_r0_r1() {
+        let insn_bytes: u32 = 0x0800;
+        let insn = Instruction::disassemble(insn_bytes);
+
+        assert_eq!(insn.opcode, OpCode::LDW);
+        assert_eq!(insn.op0, 0);
+        assert_eq!(insn.op1, 1);
+    }
+
+    #[test]
+    fn test_instruction_disassemble_xor_r2_r3() {
+        let insn_bytes: u32 = 0x1883;
+        let insn = Instruction::disassemble(insn_bytes);
+
+        assert_eq!(insn.opcode, OpCode::XOR);
+        assert_eq!(insn.op0, 2);
+        assert_eq!(insn.op1, 3);
+    }
+
+    #[test]
+    fn test_instruction_disassemble_stw_r5_r0() {
+        let insn_bytes: u32 = 0x0141;
+        let insn = Instruction::disassemble(insn_bytes);
+
+        assert_eq!(insn.opcode, OpCode::STW);
+        assert_eq!(insn.op0, 5);
+        assert_eq!(insn.op1, 0);
+    }
 }
