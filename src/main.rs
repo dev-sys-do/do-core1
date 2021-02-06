990fc8a06a5a0c5f38d42a682c7331e9ca30403e
Samuel Ortiz <sameo@linux.intel.com>
2021-02-06 16:12:09 +0100

    do-core1: Add OpCode Enumeration
    Introduce Rust's versatile Enum for defining a new type: OpCode.
    First pattern matching code path.
    Pretty print the Enum value.
    
    Signed-off-by: Samuel Ortiz <sameo@linux.intel.com>

--- src/main.rs
+++ src/main.rs
@@ -1,26 +1,32 @@
+#[derive(Debug)]
+enum OpCode {
+    LDW = 0x00,
+    STW = 0x01,
+    ADD = 0x02,
+    XOR = 0x03,
+}
+
 fn add(op0: u32, op1: u32) -> u32 {
     op0 + op1
 }
 
 fn main() {
-    let opcode: u8 = 0x02;
+    let opcode: OpCode = OpCode::ADD;
     let mut r0: u32 = 10;
     let r1: u32 = 20;
 
     println!(
-        "do-core-1: opcode {} Initial CPU state [R0:{} R1:{}]",
+        "do-core-1: opcode {:?} Initial CPU state [R0:{} R1:{}]",
         opcode, r0, r1
     );
 
-    if opcode == 0x02 {
-        // ADD r0, r1
-        r0 = add(r0, r1);
-    } else {
-        panic!("Unknown opcode {}", opcode);
+    match opcode {
+        OpCode::ADD => r0 = add(r0, r1),
+        _ => panic!("Unknown opcode {:?}", opcode),
     }
 
     println!(
-        "do-core-1: opcode {} Final CPU state [R0:{} R1:{}]",
+        "do-core-1: opcode {:?} Final CPU state [R0:{} R1:{}]",
         opcode, r0, r1
     );
 }
