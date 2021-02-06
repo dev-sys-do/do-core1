8b94d7829c72d78441b0217d00d02bf577b21b3b
Samuel Ortiz <sameo@linux.intel.com>
2021-02-06 16:14:48 +0100

    do-core1: Implement the XOR instruction
    Live exercice #1.
    
    Signed-off-by: Samuel Ortiz <sameo@linux.intel.com>

--- src/main.rs
+++ src/main.rs
@@ -11,6 +11,10 @@ fn add(op0: u16, op1: u16) -> u16 {
     op0 + op1
 }
 
+fn xor(op0: u16, op1: u16) -> u16 {
+    op0 ^ op1
+}
+
 fn main() {
     let opcode: OpCode = OpCode::ADD;
     let mut r0: u16 = 10;
@@ -23,6 +27,7 @@ fn main() {
 
     match opcode {
         OpCode::ADD => r0 = add(r0, r1),
+        OpCode::XOR => r0 = xor(r0, r1),
         _ => panic!("Unknown opcode {:?}", opcode),
     }
 
