ec80d40549f470705fc4ffadf356f9850349701e
Samuel Ortiz <sameo@linux.intel.com>
2021-02-06 15:30:22 +0100

    do-core1: 2-operand addition
    Introduce mutability by adding the addition result to the first operand.
    The first operand must be defined as mutable, or else we fail to build.
    
    Signed-off-by: Samuel Ortiz <sameo@linux.intel.com>

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,9 @@
 fn main() {
-    let r0: u16 = 10;
+    let mut r0: u16 = 10;
     let r1: u16 = 20;
-    let result = r0 + r1;
 
-    println!("do-core-1: {}", result);
+    // ADD r0, r1
+    r0 = r0 + r1;
+
+    println!("do-core-1: {}", r0);
 }
