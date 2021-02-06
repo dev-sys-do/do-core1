bfc618ae520681180bcdde45786d16adf3f2a83b
Samuel Ortiz <sameo@linux.intel.com>
2021-02-06 15:59:57 +0100

    do-core1: Check for the only supported opcode
    Introduce Rust flow control (i.e. if) in order to check that we add only
    when the opcode is ADD. Panic! otherwise.
    
    Signed-off-by: Samuel Ortiz <sameo@linux.intel.com>

--- src/main.rs
+++ src/main.rs
@@ -3,11 +3,24 @@ fn add(op0: u32, op1: u32) -> u32 {
 }
 
 fn main() {
+    let opcode: u8 = 0x02;
     let mut r0: u32 = 10;
     let r1: u32 = 20;
 
-    // ADD r0, r1
-    r0 = add(r0, r1);
+    println!(
+        "do-core-1: opcode {} Initial CPU state [R0:{} R1:{}]",
+        opcode, r0, r1
+    );
 
-    println!("do-core-1: {}", r0);
+    if opcode == 0x02 {
+        // ADD r0, r1
+        r0 = add(r0, r1);
+    } else {
+        panic!("Unknown opcode {}", opcode);
+    }
+
+    println!(
+        "do-core-1: opcode {} Final CPU state [R0:{} R1:{}]",
+        opcode, r0, r1
+    );
 }
