c21cb868b4c4bacbc8d79c9a1f9af368b1ed509d
Samuel Ortiz <sameo@linux.intel.com>
2021-02-06 20:06:38 +0100

    do-core1: Add a routine for dumping a do-core1 CPU state
    Now that we have a complete set of registers, it is more convenient to
    be able to print all of them.
    
    Signed-off-by: Samuel Ortiz <sameo@linux.intel.com>

--- src/main.rs
+++ src/main.rs
@@ -68,6 +68,13 @@ fn xor(op0: u16, op1: u16) -> u16 {
     op0 ^ op1
 }
 
+fn dump_cpu_state(preamble: &str, registers: &[u16; MAX_REGISTER_INDEX as usize + 1]) {
+    println!("do-core1: {}:", preamble);
+    for (index, register) in registers.iter().enumerate() {
+        println!("\tR{}: {:#x?}", index, *register);
+    }
+}
+
 fn main() -> Result<(), Error> {
     let arguments = App::new("do-core1")
         .about("do-core1 emulator")
@@ -88,10 +95,7 @@ fn main() -> Result<(), Error> {
     let insn = u16::from_str_radix(insn_string, 16).unwrap();
     let mut registers = [0; MAX_REGISTER_INDEX as usize + 1];
 
-    println!(
-        "do-core-1: instruction {:#x?} Initial CPU state {:#x?}",
-        insn, registers
-    );
+    dump_cpu_state("Initial CPU state", &registers);
 
     let decoded_instruction = Instruction::disassemble(insn)?;
     println!(
@@ -107,10 +111,7 @@ fn main() -> Result<(), Error> {
         _ => panic!("Unknown opcode {:?}", decoded_instruction.opcode),
     }
 
-    println!(
-        "do-core-1: instruction {:#x?} Initial CPU state {:#x?}",
-        insn, registers
-    );
+    dump_cpu_state("Final CPU state", &registers);
 
     Ok(())
 }
