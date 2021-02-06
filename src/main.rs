9d576e6e12369c11f4bff6270fd494fd73be11bd
Samuel Ortiz <sameo@linux.intel.com>
2021-02-06 15:36:37 +0100

    do-core1: Define an addition function
    Introduce the function syntax to abstract registers addition.
    
    Signed-off-by: Samuel Ortiz <sameo@linux.intel.com>

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,13 @@
+fn add(op0: u16, op1: u16) -> u16 {
+    op0 + op1
+}
+
 fn main() {
     let mut r0: u16 = 10;
     let r1: u16 = 20;
 
     // ADD r0, r1
-    r0 = r0 + r1;
+    r0 = add(r0, r1);
 
     println!("do-core-1: {}", r0);
 }
