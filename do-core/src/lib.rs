ee617112e15546ed519d4c2fe53d13b161cee331
Samuel Ortiz <sameo@linux.intel.com>
2021-03-08 00:13:51 +0100

    do-core1: Move our core implementations to do-core
    Signed-off-by: Samuel Ortiz <s.ortiz@apple.com>

--- do-core/src/lib.rs
+++ do-core/src/lib.rs
@@ -1,6 +1,9 @@
+use crate::instruction::OpCode;
+
 #[derive(Debug)]
 pub enum Error {
     InvalidOpCode(u8),
+    UnsupportedOpCode(OpCode),
     Op0OutOfRange,
     Op1OutOfRange,
     AdditionOverflow(u32, u32),
@@ -9,4 +12,5 @@ pub enum Error {
 // do-core1 register indexes range from 0 to 31.
 pub const MAX_REGISTER_INDEX: u8 = 31;
 
+pub mod core;
 pub mod instruction;
--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,6 @@
 use clap::Parser;
-use do_core::instruction::{Instruction, OpCode};
-use do_core::{Error, MAX_REGISTER_INDEX};
+use do_core::core::Core;
+use do_core::Error;
 
 #[derive(Parser)]
 #[clap(version, author)]
@@ -10,49 +10,16 @@ struct DoCoreOpts {
     insn: String,
 }
 
-fn add(op0: u32, op1: u32) -> Result<u32, Error> {
-    op0.checked_add(op1)
-        .ok_or(Error::AdditionOverflow(op0, op1))
-}
-
-fn xor(op0: u32, op1: u32) -> u32 {
-    op0 ^ op1
-}
-
-fn dump_cpu_state(preamble: &str, registers: &[u32; MAX_REGISTER_INDEX as usize + 1]) {
-    println!("do-core1: {}:", preamble);
-    for (index, register) in registers.iter().enumerate() {
-        println!("\tR{}: {:#x?}", index, *register);
-    }
-}
-
 fn main() -> Result<(), Error> {
+    let mut cpu = Core::new();
     let opts: DoCoreOpts = DoCoreOpts::parse();
-    let insn = u32::from_str_radix(opts.insn.trim_start_matches("0x"), 16).unwrap();
-    let mut registers = [0u32; MAX_REGISTER_INDEX as usize + 1];
-    // Arbitrary initial registers value.
-    // Registers will eventually be initialized through memory loads.
-    for (index, register) in registers.iter_mut().enumerate() {
-        *register = index as u32 * 0x10;
-    }
 
-    dump_cpu_state("Initial CPU state", &registers);
+    cpu.dump("Initial CPU state");
 
-    let decoded_instruction = Instruction::disassemble(insn)?;
-    println!(
-        "do-core-1: instruction decoded into {:?}",
-        decoded_instruction
-    );
-    let op0 = decoded_instruction.op0() as usize;
-    let op1 = decoded_instruction.op1() as usize;
+    let insn = cpu.decode(u32::from_str_radix(opts.insn.trim_start_matches("0x"), 16).unwrap())?;
+    cpu.execute(insn)?;
 
-    match decoded_instruction.opcode() {
-        OpCode::ADD => registers[op0] = add(registers[op0], registers[op1])?,
-        OpCode::XOR => registers[op0] = xor(registers[op0], registers[op1]),
-        _ => panic!("Unknown opcode {:?}", decoded_instruction.opcode()),
-    }
-
-    dump_cpu_state("Final CPU state", &registers);
+    cpu.dump("Final CPU state");
 
     Ok(())
 }
