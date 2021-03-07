a6f2e36f17afcff8fbf80227188317943c7358ee
Samuel Ortiz <sameo@linux.intel.com>
2021-03-07 23:43:35 +0100

    do-core1: Move Instruction to the do-core crate
    Signed-off-by: Samuel Ortiz <sameo@linux.intel.com>

--- do-core/src/instruction.rs
+++ do-core/src/instruction.rs
@@ -1,4 +1,4 @@
-use crate::Error;
+use crate::{Error, MAX_REGISTER_INDEX};
 
 #[allow(dead_code)]
 #[derive(Clone, Debug, PartialEq)]
@@ -18,5 +18,36 @@ impl OpCode {
             0x03 => Ok(OpCode::XOR),
             _ => Err(Error::InvalidOpCode(opcode)),
         }
+    }
+}
+
+#[derive(Debug)]
+pub struct Instruction {
+    opcode: OpCode,
+    op0: u8,
+    op1: u8,
+}
+
+impl Instruction {
+    // Instruction constructor, a.k.a. disassembler.
+    pub fn disassemble(insn: u32) -> Result<Instruction, Error> {
+        // Keep the first 6 bits only
+        let opcode = OpCode::from_u8((insn & 0x3f) as u8)?;
+
+        // Shift right by 6, keep only the first 5 bits.
+        let op0 = ((insn >> 6) & 0x1f) as u8;
+
+        // Shift right by 11, keep only the first 5 bits.
+        let op1: u8 = ((insn >> 11) & 0x1f) as u8;
+
+        if op0 > MAX_REGISTER_INDEX {
+            return Err(Error::Op0OutOfRange);
+        }
+
+        if op1 > MAX_REGISTER_INDEX {
+            return Err(Error::Op1OutOfRange);
+        }
+
+        Ok(Instruction { opcode, op0, op1 })
     }
 }
