0aab1f4a29bfef38c31356a77ce494e321eb5a14
Samuel Ortiz <sameo@linux.intel.com>
2021-03-07 23:32:37 +0100

    do-core1: Initial do-core crate
    With a minimal content, just our opcodes.
    
    Signed-off-by: Samuel Ortiz <sameo@linux.intel.com>

---
+++ do-core/src/instruction.rs
@@ -0,0 +1,22 @@
+use crate::Error;
+
+#[allow(dead_code)]
+#[derive(Clone, Debug, PartialEq)]
+pub enum OpCode {
+    LDW = 0x00,
+    STW = 0x01,
+    ADD = 0x02,
+    XOR = 0x03,
+}
+
+impl OpCode {
+    pub fn from_u8(opcode: u8) -> Result<OpCode, Error> {
+        match opcode {
+            0x00 => Ok(OpCode::LDW),
+            0x01 => Ok(OpCode::STW),
+            0x02 => Ok(OpCode::ADD),
+            0x03 => Ok(OpCode::XOR),
+            _ => Err(Error::InvalidOpCode(opcode)),
+        }
+    }
+}
