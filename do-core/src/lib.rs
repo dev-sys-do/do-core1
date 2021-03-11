e4ec94386079eb3b6524233957a4a8facc09d210
Samuel Ortiz <sameo@linux.intel.com>
2021-03-11 20:13:45 +0100

    do-core: core: Implement LDW and STW
    Signed-off-by: Samuel Ortiz <s.ortiz@apple.com>

--- do-core/src/lib.rs
+++ do-core/src/lib.rs
@@ -13,6 +13,9 @@ pub enum Error {
 // do-core1 register indexes range from 0 to 31.
 pub const MAX_REGISTER_INDEX: u8 = 31;
 
+// do-core only support 4K of memory
+pub const MEMORY_SIZE: usize = 0x1000;
+
 pub mod core;
 pub mod instruction;
 pub mod memory;
