c5b39efca21c37945f800bc2c42873f5fccf3cc9
Samuel Ortiz <sameo@linux.intel.com>
2021-03-11 20:13:45 +0100

    do-core: core: Implement LD and ST
    Signed-off-by: Samuel Ortiz <sameo@linux.intel.com>

--- do-core/src/lib.rs
+++ do-core/src/lib.rs
@@ -7,10 +7,14 @@ pub enum Error {
     Op0OutOfRange,
     Op1OutOfRange,
     AdditionOverflow(u16, u16),
+    MemoryOverflow(u16),
 }
 
 // do-core register indexes range from 0 to 7.
 pub const MAX_REGISTER_INDEX: u8 = 7;
+
+// do-core only support 4K of memory
+pub const MEMORY_SIZE: usize = 0x1000;
 
 pub mod core;
 pub mod instruction;
