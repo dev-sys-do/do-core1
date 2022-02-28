fbcd1980e494f1e65657672d591f7dffd3bf5f0d
iverly <iverly.dev@gmail.com>
2022-02-28 12:18:46 +0100

    feat: add cli with args parser

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,19 @@
+use clap::Parser;
+
 #[derive(Debug)]
 struct Instruction {
     opcode: OpCode,
     op0: u8,
     op1: u8,
+}
+
+// Simple cli to execute an instruction from the do-core1
+#[derive(Parser, Debug)]
+#[clap(author, version, about, long_about = None)]
+struct DoCoreArgs {
+    /// Instruction to execute
+    #[clap(short, long)]
+    insn: String,
 }
 
 impl Instruction {
@@ -72,7 +83,8 @@ fn main() {
     // 0001 1000 0100 0010
     //  1     8   4    2
     // 0b0001100001000010 = 0x1842
-    let insn: u32 = 0x1842;
+    let args: DoCoreArgs = DoCoreArgs::parse();
+    let insn: u32 = u32::from_str_radix(args.insn.trim_start_matches("0x"), 16).unwrap();
     let mut r1: u32 = 20;
     let r3: u32 = 12;
 
