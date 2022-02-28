35ebf2aa0476d07164178eebda233bdee470b9e2
Julien Dubois <dubois.julien.mail@gmail.com>
2022-02-28 11:02:49 +0100

    d0-core1: Implement SHL and SHR instructions

--- src/main.rs
+++ src/main.rs
@@ -28,6 +28,8 @@ enum OpCode {
     STW = 0x01,
     ADD = 0x02,
     XOR = 0x03,
+    SHL = 0x04,
+    SHR = 0x05,
 }
 
 impl OpCode {
@@ -37,6 +39,8 @@ impl OpCode {
             0x01 => OpCode::STW,
             0x02 => OpCode::ADD,
             0x03 => OpCode::XOR,
+            0x04 => OpCode::SHL,
+            0x05 => OpCode::SHR,
             _ => panic!("Unknown opcode {:?}", opcode),
         }
     }
@@ -47,6 +51,22 @@ fn add(op0: u32, op1: u32) -> u32 {
 
 fn xor(op0: u32, op1: u32) -> u32 {
     op0 ^ op1
+}
+
+fn shift_left(op0: u32, op1: u32) -> u32 {
+    if op1 < 32 {
+        op0 << op1
+    } else {
+        panic!("Attempt to shift left by {:?} which would overflow", op1);
+    }
+}
+
+fn shift_right(op0: u32, op1: u32) -> u32 {
+    if op1 < 32 {
+        op0 >> op1
+    } else {
+        panic!("Attempt to shift right by {:?} which would overflow", op1);
+    }
 }
 
 fn main() {
@@ -78,6 +98,8 @@ fn main() {
     match decoded_instruction.opcode {
         OpCode::ADD => r1 = add(r1, r3),
         OpCode::XOR => r1 = xor(r1, r3),
+        OpCode::SHL => r1 = shift_left(r1, r3),
+        OpCode::SHR => r1 = shift_right(r1, r3),
         _ => panic!("Unknown opcode {:?}", decoded_instruction.opcode),
     }
 
@@ -92,11 +114,61 @@ mod tests {
     use crate::{Instruction, OpCode};
 
     #[test]
+    fn test_instruction_disassemble_ldw_r1_r3() {
+        let insn_bytes: u32 = 0x1840;
+        let insn = Instruction::disassemble(insn_bytes);
+
+        assert_eq!(insn.opcode, OpCode::LDW);
+        assert_eq!(insn.op0, 1);
+        assert_eq!(insn.op1, 3);
+    }
+
+    #[test]
+    fn test_instruction_disassemble_stw_r1_r3() {
+        let insn_bytes: u32 = 0x1841;
+        let insn = Instruction::disassemble(insn_bytes);
+
+        assert_eq!(insn.opcode, OpCode::STW);
+        assert_eq!(insn.op0, 1);
+        assert_eq!(insn.op1, 3);
+    }
+
+    #[test]
     fn test_instruction_disassemble_add_r1_r3() {
         let insn_bytes: u32 = 0x1842;
         let insn = Instruction::disassemble(insn_bytes);
 
         assert_eq!(insn.opcode, OpCode::ADD);
+        assert_eq!(insn.op0, 1);
+        assert_eq!(insn.op1, 3);
+    }
+
+    #[test]
+    fn test_instruction_disassemble_xor_r1_r3() {
+        let insn_bytes: u32 = 0x1843;
+        let insn = Instruction::disassemble(insn_bytes);
+
+        assert_eq!(insn.opcode, OpCode::XOR);
+        assert_eq!(insn.op0, 1);
+        assert_eq!(insn.op1, 3);
+    }
+
+    #[test]
+    fn test_instruction_disassemble_shift_left_r1_r3() {
+        let insn_bytes: u32 = 0x1844;
+        let insn = Instruction::disassemble(insn_bytes);
+
+        assert_eq!(insn.opcode, OpCode::SHL);
+        assert_eq!(insn.op0, 1);
+        assert_eq!(insn.op1, 3);
+    }
+
+    #[test]
+    fn test_instruction_disassemble_shift_right_r1_r3() {
+        let insn_bytes: u32 = 0x1845;
+        let insn = Instruction::disassemble(insn_bytes);
+
+        assert_eq!(insn.opcode, OpCode::SHR);
         assert_eq!(insn.op0, 1);
         assert_eq!(insn.op1, 3);
     }
