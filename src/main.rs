3f10e9cdd11bbed7033125a2eb09ab002dbb647d
iverly <iverly.dev@gmail.com>
2022-02-28 11:27:35 +0100

    feat: add shr/shl instructions

--- src/main.rs
+++ src/main.rs
@@ -28,6 +28,8 @@ enum OpCode {
     STW = 0x01,
     ADD = 0x02,
     XOR = 0x03,
+    SHR = 0x04,
+    SHL = 0x05,
 }
 
 impl OpCode {
@@ -37,6 +39,8 @@ impl OpCode {
             0x01 => OpCode::STW,
             0x02 => OpCode::ADD,
             0x03 => OpCode::XOR,
+            0x04 => OpCode::SHR,
+            0x05 => OpCode::SHL,
             _ => panic!("Unknown opcode {:?}", opcode),
         }
     }
@@ -47,6 +51,14 @@ fn add(op0: u32, op1: u32) -> u32 {
 
 fn xor(op0: u32, op1: u32) -> u32 {
     op0 ^ op1
+}
+
+fn shr(op0: u32, op1: u8) -> u32 {
+    op0 >> op1
+}
+
+fn shl(op0: u32, op1: u8) -> u32 {
+    op0 << op1
 }
 
 fn main() {
@@ -78,6 +90,8 @@ fn main() {
     match decoded_instruction.opcode {
         OpCode::ADD => r1 = add(r1, r3),
         OpCode::XOR => r1 = xor(r1, r3),
+        OpCode::SHL => r1 = shr(r1, decoded_instruction.op1),
+        OpCode::SHR => r1 = shl(r1, decoded_instruction.op1),
         _ => panic!("Unknown opcode {:?}", decoded_instruction.opcode),
     }
 
@@ -139,5 +153,25 @@ mod tests {
         assert_eq!(insn.opcode, OpCode::STW);
         assert_eq!(insn.op0, 5);
         assert_eq!(insn.op1, 0);
+    }
+
+    #[test]
+    fn test_instruction_disassemble_shr_r1_r0() {
+        let insn_bytes: u32 = 0x0804;
+        let insn = Instruction::disassemble(insn_bytes);
+
+        assert_eq!(insn.opcode, OpCode::SHR);
+        assert_eq!(insn.op0, 0);
+        assert_eq!(insn.op1, 1);
+    }
+
+    #[test]
+    fn test_instruction_disassemble_shl_r6_r2() {
+        let insn_bytes: u32 = 0x1185;
+        let insn = Instruction::disassemble(insn_bytes);
+
+        assert_eq!(insn.opcode, OpCode::SHL);
+        assert_eq!(insn.op0, 6);
+        assert_eq!(insn.op1, 2);
     }
 }
